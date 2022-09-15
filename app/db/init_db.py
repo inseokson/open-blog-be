b6069b7b82d3b88fcb9eb5ec28b45e120eaf6987
inseokson <inseokson92@gmail.com>
2022-09-15 22:30:57 +0900

    Style: Code & Import Styling

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,7 +1,7 @@
 # Import the relevant models where you call Base.metadata.create_all
 # for more details:
 # https://stackoverflow.com/questions/54118182/sqlalchemy-not-creating-tables
-from .base import Base, Post
+from .base import Base, Post  # noqa
 from .session import engine
 
 
